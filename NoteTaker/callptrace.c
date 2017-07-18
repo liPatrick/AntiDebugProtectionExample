b244eb71cfc4fa3eda3e2c80a93f0f892ee825e5
Patrick Li <pali@itrus-MacBook-Pro-2.local>
2017-07-18 15:42:41 -0700

    first commit

---
+++ NoteTaker/callptrace.c
@@ -0,0 +1,9 @@
+//
+//  callptrace.c
+//  NoteTaker
+//
+//  Created by Patrick Li on 7/18/17.
+//  Copyright © 2017 Shane Doyle. All rights reserved.
+//
+
+#include "callptrace.h"
---
+++ NoteTaker/callptrace.h
@@ -0,0 +1,14 @@
+//
+//  callptrace.h
+//  NoteTaker
+//
+//  Created by Patrick Li on 7/18/17.
+//  Copyright © 2017 Shane Doyle. All rights reserved.
+//
+
+#ifndef callptrace_h
+#define callptrace_h
+
+#include <stdio.h>
+
+#endif /* callptrace_h */
